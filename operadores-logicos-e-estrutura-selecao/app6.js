a06a6723d6fd720ee0578cd99603f54b45158c51
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-04-24 17:54:09 -0300

    operador elseif

---
+++ operadores-logicos-e-estrutura-selecao/app6.js
@@ -0,0 +1,11 @@
+// elseif - executada se o if falhar, verifica se a condição inteira passada entre parênteses é verdadeira
+// se sim, executa o trecho
+
+const valido = false;
+const invalido =true;
+
+if (valido){
+    console.log("Válido");
+} else if (invalido){
+    console.log("Inválido");
+}
