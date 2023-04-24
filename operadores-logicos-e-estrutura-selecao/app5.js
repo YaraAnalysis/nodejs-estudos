dafcd52afc4d9279eb38abddc6b3b43a6d641b71
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-04-23 22:50:10 -0300

    Exercicio IF

---
+++ operadores-logicos-e-estrutura-selecao/app5.js
@@ -0,0 +1,10 @@
+// if - verifica se a condição inteira passada entre parênteses é verdadeira. Se sim, executa o trecho.
+// Se não, verifica se foi definida uma cláusula ESLE e a executa.
+
+const valido = false;
+
+if (valido){
+    console.log(valido);
+}else {
+    console.log(valido);
+}
